6abb021bb3635296b2192956f476cf5e0f8227b9
Blearybear <timh2026@student.cis.edu.hk>
2024-01-26 09:14:42 +0800

    Finished CS Unit 7 Tutorial Song [v1]

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,35 @@
-public class Song {
+import java.util.ArrayList;
 
-    private String
+public class Song {
+    private String title;
+    private String artist;
+    private ArrayList<String> pastListeners;
+    public Song(String title, String artist){
+        this.pastListeners = new ArrayList<>();
+        this.title = title;
+        this.artist = artist;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany(ArrayList<String> arrayList) {
+        int count = 0;
+        for (int i = 0; i < arrayList.size(); i++) {
+            boolean isNewListener = true;
+            for (int e = 0; e < this.pastListeners.size(); e++) {
+                if (arrayList.get(i).toLowerCase().equals(pastListeners.get(e).toLowerCase())) {
+                    isNewListener = false;
+                    break;
+                }
+            }
+            if (isNewListener) {
+                pastListeners.add(arrayList.get(i));
+                count++;
+            }
+        }
+        return count;
+    }
 }
